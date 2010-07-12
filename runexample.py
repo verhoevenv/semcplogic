a499ce9e17ecaaa405c622db15ecd185c3919a68
Vincent Verhoeven <verhoevenv@gmail.com>
2010-07-12 22:39:57 +0200

    Made runexample actually work. I believe so anyway.

--- runexample.py
+++ runexample.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# a testament to all the hoops Python makes us jump through
+
 import inspect
 import runpy
 import sys
+import os.path
 
 if len(sys.argv) < 2:
   print "Usage: runexample.py <filename>"
-  print "Example: runexample.py examples/example_data.py"
+  print "Example: runexample.py examples/analysis.py"
   sys.exit()
 
 f = sys.argv[1]
 module = inspect.getmodulename(f)
-if module == None:
+if module is None:
   module = f
+else:
+  module = "%s.%s" % (f[:f.rfind(os.path.sep)], module)
 runpy.run_module(module)
